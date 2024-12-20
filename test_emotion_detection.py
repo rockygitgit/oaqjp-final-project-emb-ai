76030a4eb317b628a081b3e4148e4dd09799dba7
rockygitgit <76917152+rockygitgit@users.noreply.github.com>
2024-12-20 22:43:18 +0800

    Create test_emotion_detection.py

---
+++ test_emotion_detection.py
@@ -0,0 +1,22 @@
+import unittest 
+import json
+from EmotionDetection.emotion_detection import emotion_detector
+
+class TestEmotionDectetor(unittest.TestCase):
+    def test_emotion_detector(self):
+        result_1 = json.loads(emotion_detector('I am glad this happened'))
+        self.assertEqual(result_1['dominant_emotion'], 'joy')
+    
+        result_1 = json.loads(emotion_detector('I am really mad about this'))
+        self.assertEqual(result_1['dominant_emotion'], 'anger')
+    
+        result_1 = json.loads(emotion_detector('I feel disgusted just hearing about this'))
+        self.assertEqual(result_1['dominant_emotion'], 'disgust')
+    
+        result_1 = json.loads(emotion_detector('I am so sad about this'))
+        self.assertEqual(result_1['dominant_emotion'], 'sadness')
+    
+        result_1 = json.loads(emotion_detector('I am really afraid that this will happen'))
+        self.assertEqual(result_1['dominant_emotion'], 'fear')
+    
+unittest.main()
